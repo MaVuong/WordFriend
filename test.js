436f7d14d7c408e1c6aa65126bd71ce88952e6bb
MaVuong <dethichthien@me.com>
2018-04-13 16:18:30 +0700

    Update test.js

--- test.js
+++ test.js
@@ -4,5 +4,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+}).listen(80); //the server object listens on port 8080
 console.log("---server is starting");
